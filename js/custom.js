e3ec944f47dbbcf60c6cc904ab1d1edc3cc519b8
Marc Lo <lo.hungyui@gmail.com>
2015-11-02 03:27:00 -0500

    interactive timeline

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+  var options = [
+      {
+          selector: '#work-experience-timeline',
+          offset: 500,
+          callback: 'Materialize.showStaggeredList("#work1-card")'
+      },
+      {
+          selector: '#work-experience-timeline',
+          offset: 800,
+          callback: 'Materialize.showStaggeredList("#work2-card")'
+      },
+      {
+          selector: '#work-experience-timeline',
+          offset: 1100,
+          callback: 'Materialize.showStaggeredList("#work3-card")'
+      }
+  ];
+  Materialize.scrollFire(options);
