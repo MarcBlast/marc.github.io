03af9b8186854553e90d535f2682c6ae461b89b7
Marc Lo <lo.hungyui@gmail.com>
2015-11-02 23:01:54 -0500

    test for parallax

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,22 @@
-  var options = [
-      {
-          selector: '#work-experience-timeline',
-          offset: 500,
-          callback: 'Materialize.showStaggeredList("#work1-card")'
-      },
-      {
-          selector: '#work-experience-timeline',
-          offset: 800,
-          callback: 'Materialize.showStaggeredList("#work2-card")'
-      },
-      {
-          selector: '#work-experience-timeline',
-          offset: 1100,
-          callback: 'Materialize.showStaggeredList("#work3-card")'
-      }
-  ];
-  Materialize.scrollFire(options);
+var options = [
+    {
+        selector: '#work-experience-timeline',
+        offset: 500,
+        callback: 'Materialize.showStaggeredList("#work1-card")'
+  },
+    {
+        selector: '#work-experience-timeline',
+        offset: 800,
+        callback: 'Materialize.showStaggeredList("#work2-card")'
+  },
+    {
+        selector: '#work-experience-timeline',
+        offset: 1100,
+        callback: 'Materialize.showStaggeredList("#work3-card")'
+  }
+];
+Materialize.scrollFire(options);
+
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
